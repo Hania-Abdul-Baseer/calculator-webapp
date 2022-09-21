291137dfa2fe570a97cc3bc1a7f25d013b8b1b35
Hania-Abdul-Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-09-21 12:30:09 +0930

    updating style.css and index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const numButtons = document.querySelectorAll('[data-number');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOutput = document.querySelector('[data-previous-output]');
+const currentOutput = document.querySelector('[data-current-output]');
